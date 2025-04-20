ba9ccc9cea631dafadf5fc9196694274281ff1ec
MichalTecl <mtecl.prg@gmail.com>
2025-04-20 20:24:44 +0200

    gps picking, distributros grid

--- Elsa.App.Crm/Model/DistributorGridRowModel.cs
+++ Elsa.App.Crm/Model/DistributorGridRowModel.cs
@@ -1,7 +1,8 @@
-﻿using Elsa.Common.Utils;
+using Elsa.Common.Utils;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,9 @@ namespace Elsa.App.Crm.Model
         public DateTime? LastContactDt { get; set; }
         public DateTime? FutureContactDt { get; set; }
         public int TotalOrdersCount { get; set; }
-        public decimal TotalOrdersTaxedPrice { get; set; }       
+        public decimal TotalOrdersUntaxedPrice { get; set; }       
+        public string TotalOrdersPriceF => TotalOrdersUntaxedPrice.ToString("N0", new CultureInfo("cs-CZ"));
+
         [JsonIgnore]
         public string SearchTag { get; set; }
         public List<SalesTrendTick> TrendModel { get; set; }
